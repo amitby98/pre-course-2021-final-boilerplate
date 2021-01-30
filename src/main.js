f4d16771d9fb0bb3b61de5ed7704733f0c8a843e
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-01-30 02:27:21 +0200

    edited the sort button

--- src/main.js
+++ src/main.js
@@ -14,14 +14,18 @@ filterButton.addEventListener("click", (e) => {
     filter += 1;
     filterButton.innerText = "High importance";
     sortByImportance(true);
-  } else if (filter >= 2) {
+  } else if (filter === 2) {
     filterButton.innerText = "Low importance";
     sortByImportance(false);
+    filter += 1;
+  } else if (filter >= 3) {
+    filterButton.innerText = "Type of task";
+    sortByType(true);
     filter = 0;
   }
 });
 
-//function to sort tasks by importance.
+//function to sort tasks by importance
 function sortByImportance(increase) {
   let todoArray = document.getElementsByClassName("todo-container");
   let tasksContainer = document.getElementById("tasks-container");
@@ -80,10 +84,48 @@ function sortByImportance(increase) {
   }
 }
 
-// //function to sort all tasks by their type.
-// function sortByType() {
-//   let divs = document.getElementsByClassName("todo-container");
-// }
+//function to sort all tasks by their type
+function sortByType(increase) {
+  let todoArray = document.getElementsByClassName("todo-container");
+  let tasksContainer = document.getElementById("tasks-container");
+  let typeArray1 = [];
+  let typeArray2 = [];
+  let typeArray3 = [];
+  for (i = 0; i < todoArray.length; i++) {
+    let taskDiv = todoArray[i]; //Task div
+    let type = taskDiv.getElementsByTagName("div")[3].innerText;
+    switch (type) {
+      case "Must":
+        typeArray1.push(taskDiv);
+        taskDiv.remove;
+        break;
+      case "Quickly":
+        typeArray2.push(taskDiv);
+        taskDiv.remove;
+        break;
+      case "Normal":
+        typeArray3.push(taskDiv);
+        taskDiv.remove;
+        break;
+    }
+  }
+  let types = [typeArray1, typeArray2, typeArray3];
+  let organized = [];
+  for (let i = 0; i < types.length; i++) {
+    if (types[i].length > 0) {
+      for (let z = 0; z < types[i].length; z++) {
+        if (increase) {
+          organized.push(types[i][z]);
+        } else {
+          organized.unshift(types[i][z]);
+        }
+      }
+    }
+  }
+  for (let i = 0; i < organized.length; i++) {
+    tasksContainer.appendChild(organized[i]);
+  }
+}
 
 //function for counter
 function counterUpdated() {
@@ -117,6 +159,7 @@ function addTask() {
 
   //Find importance number
   let priorityInput = document.getElementById("priority-selector").value;
+  let typeInput = document.getElementById("priority-selector2").value;
   if (priorityInput < 1 || priorityInput > 5 || task.length < 1) {
     alert("You need to fill all the fields!");
   } else {
@@ -156,11 +199,18 @@ function addTask() {
       counterUpdated();
     });
 
+    //add type div
+    let divType = document.createElement("div");
+    divType.className = typeInput;
+    divType.innerText = typeInput;
+    console.log(divType.className);
+
     // add todo elements to the todo-container
     div.appendChild(input);
     div.appendChild(divPriority);
     div.appendChild(divTaskText);
     div.appendChild(divDate);
+    div.appendChild(divType);
     div.appendChild(removeButton);
 
     // add the todo-container to the div task list
@@ -169,7 +219,7 @@ function addTask() {
     // reset inputs after adding a task
     document.getElementsByTagName("input")[0].value = "";
     document.getElementsByTagName("select")[0].value = "";
-    document.getElementsByTagName("select")[1].value = "regular";
+    document.getElementsByTagName("select")[1].value = "Normal";
 
     // update counters
     idCounter++;
@@ -230,6 +280,9 @@ function calculateTime(time) {
   let minutes = time.getMinutes();
   if (minutes < 10) {
     minutes = "0" + minutes;
+  }
+  if (hours < 10) {
+    hours = "0" + hours;
   }
   let string =
     day + "/" + month + "/" + year + " " + hours + ":" + minutes + "   ";
