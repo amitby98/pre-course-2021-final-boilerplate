a21c831ad1e3b4a3ed200a33069ba3e53f1ff5b0
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-02-15 21:46:13 +0200

    fetch then
    Changed the fetch from async await to then

--- src/utils.js
+++ src/utils.js
@@ -2,8 +2,8 @@ const API_KEY = "$2b$10$ACsxVNXnozYAgSXjbvpoK.htaNg4CTPxW4wawEI7zBnGpC6KA/AHS";
 const MY_BIN_ID = "60183cc6abdf9c556796446d";
 
 // Gets data from persistent storage by the given key and returns it
-async function getPersistent(key) {
-  let response = await fetch("https://api.jsonbin.io/v3/b/" + key + "/latest", {
+function getPersistent(key) {
+  return fetch("https://api.jsonbin.io/v3/b/" + key + "/latest", {
     method: "GET",
     headers: {
       "X-Master-Key": API_KEY,
@@ -11,14 +11,12 @@ async function getPersistent(key) {
   });
   //console.log(response);
   //console.log(response.body);
-  const result = await response.json();
-  return result.record;
 }
 
 // Saves the given data into persistent storage by the given key.
 // Returns 'true' on success.
-async function setPersistent(key, tasks) {
-  await fetch("https://api.jsonbin.io/v3/b/" + key, {
+function setPersistent(key, tasks) {
+  return fetch("https://api.jsonbin.io/v3/b/" + key, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
@@ -26,5 +24,4 @@ async function setPersistent(key, tasks) {
     },
     body: JSON.stringify(tasks),
   });
-  return true;
 }
