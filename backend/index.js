33e4137e99603613554d36d9aa910d8375ecbe17
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-02-17 20:04:27 +0200

    added GET bin

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,42 @@
 /** @format */
 
-console.log("Hi");
 const express = require("express");
+const fs = require("fs");
 const app = express();
-app.use(express.json());
+// app.use(express.json());
 const port = 3000;
 
-app.get("/api/date/now", (req, res) => {
-  res.send("17.2.2021");
+// app.get("/api/date/now", (req, res) => {
+//   res.send("17.2.2021");
+// });
+
+// let list = {
+//   task1: "first task",
+//   task2: "second task",
+// };
+
+app.get("/v3/b/:id", (req, res) => {
+  let id = req.params.id;
+  const backendContent = fs.readFileSync(`./backend/${id}.json`);
+  res.send(backendContent);
 });
 
-app.post("/b", (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
+// app.post("/b", (req, res) => {
+//   console.log(list[id]);
+//   res.send(list[id]);
+// });
+app.put("/v3/b/:id", (req, res) => {
+  const body = req.body;
+  const id = req.params.id;
+
+  if (id) {
+    id = body;
+    res.send(id);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
-
-console.log("hi2");
