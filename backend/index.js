5cde5020ab960a684e4a23e81ede533d0c363587
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-02-19 15:17:07 +0200

    added PUT request

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const uuid = require("uuid");
 const express = require("express");
 const fs = require("fs");
 const app = express();
+const dir = "./backend";
 app.use(express.json());
 const port = 3000;
 
@@ -33,6 +34,35 @@ app.post("/v3/b", (req, res) => {
   req.body.id = uuid.v4();
   let jsonContent = JSON.stringify(req.body);
   console.log(jsonContent);
+  fs.writeFile(`./${req.body.id}.json`, jsonContent, "utf8", function (err) {
+    if (err) {
+      console.log("An error occur while writing JSON Object to File.");
+      return console.log(err);
+    }
+    console.log("JSON file has been saved.");
+  });
+  res.send(req.body);
+});
+
+app.put("/v3/b/:id", (req, res) => {
+  if (!req.body || !req.body.text || !req.body.priority) {
+    return res.status(400).json({ message: "please send correct body" });
+  }
+  if (req.body.id && req.body.id != req.params.id) {
+    return res
+      .status(400)
+      .json({ message: "please send the same id in the body and the url" });
+  }
+  if (!req.body.id) {
+    req.body.id = req.params.id;
+  }
+  let jsonContent = JSON.stringify(req.body);
+  console.log(jsonContent);
+
+  if (!fs.existsSync(`./${req.body.id}.json`)) {
+    return res.status(422).json({ message: "id is not exists" });
+  }
+
   fs.writeFile(`./${req.body.id}.json`, jsonContent, "utf8", function (err) {
     if (err) {
       console.log("An error occur while writing JSON Object to File.");
