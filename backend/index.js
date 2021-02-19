7088d9ecb73947cf1147bd264f2eff51e04a7c64
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-02-19 09:12:25 +0200

    added POST request

--- backend/index.js
+++ backend/index.js
@@ -26,6 +26,23 @@ app.delete("/v3/b/:id", (req, res) => {
   }
 });
 
+app.post("/v3/b", (req, res) => {
+  if (!req.body || !req.body.text || !req.body.priority) {
+    return res.status(400).json({ message: "please send correct body" });
+  }
+  req.body.id = uuid.v4();
+  let jsonContent = JSON.stringify(req.body);
+  console.log(jsonContent);
+  fs.writeFile(`./${req.body.id}.json`, jsonContent, "utf8", function (err) {
+    if (err) {
+      console.log("An error occur while writing JSON Object to File.");
+      return console.log(err);
+    }
+    console.log("JSON file has been saved.");
+  });
+  res.send(req.body);
+});
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
